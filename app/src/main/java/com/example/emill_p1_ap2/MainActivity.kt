e352e36acbbdd5da03bd0746b81db13cf2945d45
Emill Daniel Garcia Castellanos <Danielemill2012@gmail.com>
2023-10-11 01:26:20 -0400

    Initial commit + Dependencies + Counter for test Dependencies + ITS WORKER.

---
+++ app/src/main/java/com/example/emill_p1_ap2/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.emill_p1_ap2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.emill_p1_ap2.ui.counterscreen.CounterScreen
+import com.example.emill_p1_ap2.ui.counterscreen.CounterViewModel
+import com.example.emill_p1_ap2.ui.theme.Emill_p1_ap2Theme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Emill_p1_ap2Theme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val viewModel: CounterViewModel =  hiltViewModel()
+                    val counter by viewModel.counter.collectAsState(0)
+                    CounterScreen(
+                        counter = counter,
+                        onIncrement = viewModel::increment)
+                }
+            }
+        }
+    }
+}
