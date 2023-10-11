3cf58bd2062f2c67a8af41e619ba65037974366e
Emill Daniel Garcia Castellanos <Danielemill2012@gmail.com>
2023-10-11 19:05:24 -0400

    di/AppModule creado.

--- app/src/main/java/com/example/emill_p1_ap2/di/AppModule.kt
+++ app/src/main/java/com/example/emill_p1_ap2/di/AppModule.kt
@@ -1,6 +1,8 @@
 package com.example.emill_p1_ap2.di
 
 import android.content.Context
+import androidx.room.Room
+import com.example.emill_p1_ap2.data.DivisionDb
 import com.example.emill_p1_ap2.data.repository.CounterRepository
 import dagger.Module
 import dagger.Provides
@@ -10,9 +12,15 @@ import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
-@InstallIn ( SingletonComponent::class)
+@InstallIn(SingletonComponent::class)
 object AppModule {
-    @Singleton
     @Provides
-    fun providePreferences(@ApplicationContext context: Context) = CounterRepository(context)
+    @Singleton
+    fun provideDivisionDatabase(
+        @ApplicationContext appContext: Context
+    ): DivisionDb = Room.databaseBuilder(
+        appContext, DivisionDb::class.java, "Division.db"
+    ).fallbackToDestructiveMigration().build()
+    @Provides
+    fun providesDivisionDao(db: DivisionDb) = db.divisionDao()
 }
